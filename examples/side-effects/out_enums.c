21350e595ab85aefcbbe88aa6bad4d4b044c01f1
zmanchun <zmanchun@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-01-19 20:00:00 +0000

    cleaned up enum example
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@958 55792dc1-e732-4e53-a690-a7162b4b1b17

--- examples/side-effects/out_enums.c
+++ examples/side-effects/out_enums.c
@@ -1,21 +1,19 @@
-#include <assert.h>
-
+#include<assert.h>
+//========================= enums.c ========================
 typedef enum color{
   RED,
   BLUE
 } color_t;
-
 color_t f()
 {
   return RED;
 }
-
 int main()
 {
   int x = 0;
   enum color $sef$0 = f();
-  
-  if($sef$0 == RED){
+  if($sef$0 == RED)
+  {
     x = 1;
   }
   assert(x == 1);
