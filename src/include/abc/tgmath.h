b1d479145e26f91a5230f84143921a562b0e988e
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-04-14 19:58:15 +0000

    add tgmath.h header file
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1042 55792dc1-e732-4e53-a690-a7162b4b1b17

---
+++ src/include/abc/tgmath.h
@@ -0,0 +1,10 @@
+/* tgmath.h: The ABC representation of standard C library type generic math.
+ * Based on C11 Standard.
+ */
+#ifndef __TGMATH__
+#define __TGMATH__
+
+#include <math.h>
+#include <complex.h>
+
+#endif
