30755399cce2220c5a2023fb5904305d2c0bd93b
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-04-22 03:48:59 +0000

    add errno.h
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1066 55792dc1-e732-4e53-a690-a7162b4b1b17

---
+++ src/include/abc/errno.h
@@ -0,0 +1,12 @@
+/* errno.h: The ABC representation of standard C library.
+ * Based on C11 Standard.
+ */
+#ifndef _ERRNO_
+#define _ERRNO_
+
+#define EDOM    1
+#define EILSEQ  2
+#define ERANGE  3
+#define errno   4
+
+#endif
