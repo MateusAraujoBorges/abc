ab2700207c8b8639b592c44ddb2c4e8ea0128214
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-04-22 19:25:47 +0000

    add stdatomic.h
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1073 55792dc1-e732-4e53-a690-a7162b4b1b17

---
+++ src/include/abc/stdatomic.h
@@ -0,0 +1,13 @@
+/* stdatomic.h: The ABC representation of standard C library.
+ * Based on C11 Standard.
+ */
+ 
+#ifndef _STDATOMIC_
+#define _STDATOMIC_
+
+typedef struct atomic_flag atomic_flag;
+
+/* Macros */
+/* Functions */
+
+#endif
