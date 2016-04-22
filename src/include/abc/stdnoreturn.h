f87bfc65134fbd42a5d24692dc0b04bce326c699
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-04-22 15:37:52 +0000

    add stdnoreturn.h
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1069 55792dc1-e732-4e53-a690-a7162b4b1b17

---
+++ src/include/abc/stdnoreturn.h
@@ -0,0 +1,11 @@
+/* stdnoreturn.h: The ABC representation of standard C library.
+ * Based on C11 Standard.
+ */
+ 
+#ifndef _STDNORETURN_
+#define _STDNORETURN_
+
+/* Macros */
+#define noreturn _Noreturn;
+
+#endif
