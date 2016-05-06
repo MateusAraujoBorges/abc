c6d84ca86484278f4d25418b4574f337f13e4437
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-05-06 15:57:45 +0000

    format
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1102 55792dc1-e732-4e53-a690-a7162b4b1b17

--- src/include/abc/stdarg.h
+++ src/include/abc/stdarg.h
@@ -1,19 +1,21 @@
-#ifndef _STDARG
-#define _STDARG
+#ifndef _STDARG_
+#define _STDARG_
 
+/* Types */
 typedef struct {int id;} va_list;
 
 typedef struct {int id;} _va_arg_return_t;
 
+typedef struct {int id;} _va_param;
+
 _va_arg_return_t _va_arg(va_list val);
 
+/* Macros */
 #define va_arg(ap, type) ((type)_va_arg(ap))
 
 void va_copy(va_list dest, va_list src);
 
 void va_end(va_list ap);
-
-typedef struct {int id;} _va_param;
 
 void _va_start(va_list ap, _va_param ident);
 
