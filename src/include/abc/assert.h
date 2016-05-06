01af448fc2f3de7e2babcadaaac1f2a06cd9551d
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-05-06 18:10:46 +0000

    add doc and static_assert macro
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1107 55792dc1-e732-4e53-a690-a7162b4b1b17

--- src/include/abc/assert.h
+++ src/include/abc/assert.h
@@ -1,8 +1,16 @@
+/* The assert.h header defines assert and static_assert macros
+ * which is useful for diagnosing logic errors in the program.
+ */
+
 #ifndef _ASSERT_
 #define _ASSERT_
+
 /*@ pure;
   @ depends_on \nothing;
   @ executes_when \true;
   @ */
 $system void assert(_Bool expr);
+
+#define static_assert _Static_assert
+
 #endif
