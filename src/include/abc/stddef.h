c2f9a8b2ae5f92477c6d50a9dc9fa010a64689ed
sili <sili@55792dc1-e732-4e53-a690-a7162b4b1b17>
2016-05-06 17:20:16 +0000

    add missing types and macro.
    git-svn-id: svn://vsl.cis.udel.edu/abc/trunk@1103 55792dc1-e732-4e53-a690-a7162b4b1b17

--- src/include/abc/stddef.h
+++ src/include/abc/stddef.h
@@ -2,11 +2,21 @@
 Many of these definitions also appear in other headers. */
 #ifndef _STDDEF_
 #define _STDDEF_
-typedef unsigned long int size_t;
+
+/* Types */
 typedef signed long int ptrdiff_t;
+
+typedef unsigned long int size_t;
+
+typedef struct max_align_t max_align_t;
+
 typedef long int wchar_t;
 
+/* Macros */
 #ifndef NULL
-#define NULL ((void*)0)
+ #define NULL ((void*)0)
 #endif
+
+#define offsetof(st, m) ((size_t) ( (char *)&((st *)(0))->m - (char *)0 ))
+
 #endif
